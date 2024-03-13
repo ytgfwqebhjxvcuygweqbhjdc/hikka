a036ccb4cdaa9a04fa42e9ee15d63406b0a7b4ce
xddddddddddd =)))))) )XDXDDX :):):):):): XDXddxDXD <m733341@gmail.com>
2024-03-13 18:15:15 +0300

    Update pet.py

--- pet.py
+++ pet.py
@@ -3,12 +3,11 @@ from telethon.types import Message
 
 @loader.tds
 class PetMod(loader.Module):
-    """Pet Module by tilted"""
-    strings = { "name": "PetMod", "hello": "Hello World!"}
+    """Pet Module by tilted for моя любимая ритачян"""
+    strings = { "name": "PetMod", "newpet": "создаю нового пета...", "petname": self.get("petname", False)}
 
-    @loader.command(
-        ru_doc="Привет мир!"
-    )
-    async def helloworld(self, message: Message):
+    @loader.command(ru_doc="создает нового случайного питомца")
+    async def newpet(self, message: Message):
         """Hello World"""
-        await utils.answer(message, self.strings("hello"))
+        await utils.answer(message, self.strings("newpet"))
+        self.set("petname", message.message)
