715ca55ae52b1fdbb8cec8d7b95397af00707b75
xddddddddddd =)))))) )XDXDDX :):):):):): XDXddxDXD <m733341@gmail.com>
2024-03-13 18:04:04 +0300

    Update pet.py

---
+++ pet.py
@@ -0,0 +1,14 @@
+from .. import loader, utils
+from telethon.types import Message
+
+@loades.tds
+class PetMod(loader.Module):
+    """Pet Module by tilted"""
+    strings = { "name": "PetMod", "hello": "Hello World!"}
+
+    @loader.command(
+        ru_doc="Привет мир!"
+    )
+    async def helloworld(self, message: Message):
+        """Hello World"""
+        await utils.answer(message, self.strings("hello"))
